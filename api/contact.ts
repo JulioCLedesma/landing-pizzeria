ba329b0981f82b21bcd4fa7cf7ca6ceacf507c04
global <julio.ledesma@udgvirtual.udg.mx>
2025-11-04 18:17:27 -0600

    contact

--- api/contact.ts
+++ api/contact.ts
@@ -60,3 +60,4 @@ export default async function handler(req: VercelRequest, res: VercelResponse) {
     return res.status(500).json({ ok: false, error: "No se pudo enviar el correo." });
   }
 }
+//correso
